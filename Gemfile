ead3fe4c5f9701fc60b6660a176372474d233cfe
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2020-07-12 06:25:35 +0900

    bump version

--- Gemfile
+++ Gemfile
@@ -1,3 +1,5 @@
 source 'https://rubygems.org'
 
 gemspec :name => 'gollum-lib'
+
+gem 'gollum-rugged_adapter', github: 'pooza/rugged_adapter', branch: 'master.pooza'
--- gemspec.rb
+++ gemspec.rb
@@ -25,7 +25,7 @@ def specification(version, default_adapter, platform = nil)
     s.rdoc_options = ['--charset=UTF-8']
     s.extra_rdoc_files = %w(README.md LICENSE)
 
-    s.add_dependency *default_adapter
+    #s.add_dependency *default_adapter
     s.add_dependency 'rouge', '~> 3.1'
     s.add_dependency 'nokogiri', '~> 1.8'
     s.add_dependency 'loofah', '~> 2.3'
